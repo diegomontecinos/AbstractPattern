99c4edda14a4c613996adefe16663a4ca93dfde9
diegomontecinos <diego.montecinos.14@sansano.usm.cl>
2018-04-20 22:53:47 -0300

    base de la aplicacion mas template login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
+
+// Define the routes
+const ROUTES = [
+
+  {path: '',redirectTo: 'posts',pathMatch: 'full'},
+  {path: 'login',component: LoginComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(ROUTES) // Add routes to the app
   ],
   providers: [],
   bootstrap: [AppComponent]
