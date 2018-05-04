1175472362e0ece23c6133432edf202842c3c3c1
Diego <diego.montecinos.14@sansano.usm.cl>
2018-05-04 19:34:33 -0300

    avances en el login

--- server.js
+++ server.js
@@ -6,8 +6,27 @@ const bodyParser = require('body-parser');
 
 // Get our API routes
 const api = require('./server/routes/api');
-
 const app = express();
+
+//Mongoose conect
+const mongoose = require('mongoose');
+
+// Connected handler
+mongoose.connection.on('connected', function (err) {
+  console.log("Connected to DB using chain: " + connectionString);
+});
+
+// Error handler
+mongoose.connection.on('error', function (err) {
+  console.log(err);
+});
+
+// Reconnect when closed
+mongoose.connection.on('disconnected', function () {
+   self.connectToDatabase();
+});
+
+mongoose.connect('mongodb://admin:admin@localhost:27017/GPI');
 
 // Parsers for POST data
 app.use(bodyParser.json());
